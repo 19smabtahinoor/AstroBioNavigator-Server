3b33367327df1f9170dc11cabc7b7da0be5b9982
19smabtahinoor <smabtahinoor@gmail.com>
2025-10-04 02:35:31 +0600

    solveD

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const DEBUG = process.env.DEBUG === 'true' || false;
 
-app.use(cors({ origin: ['http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:3000', 'http://127.0.0.1:3001'], credentials: true }));
+app.use(cors({ origin: "*", credentials: true, methods: ['GET', 'POST', 'OPTIONS'], allowedHeaders: ['Content-Type'] }));
 app.use(express.json({ limit: '12mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.text({ type: ['text/*'], limit: '12mb' }));
